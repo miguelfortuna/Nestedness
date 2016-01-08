5cd7a6c32a9b874c350480edc4df539f0cb0a8de
Miguel A. Fortuna <miguelfortuna@users.noreply.github.com>
2016-01-08 12:46:14 +0100

    MatLab script: NODF and null model equiprobable.
    This MatLab file computes the NODF of a bipartite network as a metric for nestedness, and test whether the network is more nested than expected under the hypothesis that a larger or equal nested value could have been obtained by just taking into account that some species have more interactions than others (i.e., it is assumed that the probability for two species to interact is the arithmetic mean between the number of interactions each species has over the total number of potential partners).
    
    http://onlinelibrary.wiley.com/doi/10.1111/j.0030-1299.2008.16644.x/abstract

---
+++ nodf_null_model_equiprobable.m
@@ -0,0 +1,139 @@
+input_matrix; % W input matrix
+rows=size(W,1); % number of rows
+columns=size(W,2); % number of columns
+nodf_whole_matrix=zeros(1000,1); % number of replicates
+
+t=1;
+while t <= 1000 % number of replicates
+
+	%==================%
+	%--- NULL MODEL ---%
+	%==================%
+
+	input_matrix; % W input matrix
+	t
+	PR=zeros(rows,1);
+	PC=zeros(columns,1);
+	B=zeros(rows,columns);
+
+	% sum all 1s in rows and calculate intrarow probabilities 
+	for i=1:rows
+	  number_ones=0;
+	  for j=1:columns
+	    if W(i,j)==1 
+	      number_ones=number_ones+1;
+	    end
+	  end
+	  PR(i)=number_ones/columns;
+	end         
+
+	% sum all 1s in columns and calculate intracolumn probabilities
+	for j=1:columns
+	  number_ones=0;
+	  for i=1:rows 
+	    if W(i,j)==1 
+	      number_ones=number_ones+1;
+	    end
+	  end
+	  PC(j)=number_ones/rows;
+	end  
+		   
+	% randomnization of each cell average of row and column probabilities.
+	for i=1:rows
+	  for j=1:columns
+	    p(i,j)=(PR(i)+PC(j))/2; 
+	    r=rand;             
+	    if r<=p(i,j)
+	      B(i,j)=1;
+	    end
+	  end
+	end
+	B; % random matrix	
+
+	sum_cols=sum(B,1); % row vector
+	sum_rows=sum(B,2); % col vector
+	if (all(sum_cols)==1 && all(sum_rows)==1) % if the matrix size is preserved
+
+		%=============
+		%--- NODF ---%
+		%=============
+
+		W=B;
+
+		%%% nestedness columns
+		G=zeros((columns*(columns-1))/2,3); % number of different pairs of columns
+		x=0;
+		for i=1: columns
+		  for j=i+1: columns
+		    suma=0;
+		    for k=1: rows
+		      suma=suma+(W(k,i)*W(k,j));
+		    end
+		    p=nnz(W(:,i)); % number of ones in the i column
+		    q=nnz(W(:,j)); % number of ones in the j column
+		    if p > q
+		      min_column=q;
+		      x=x+1;
+		      G(x,1)=i;
+		      G(x,2)=j;
+		      G(x,3)=suma/min_column;
+		    elseif p < q
+		      min_column=p;
+		      x=x+1;
+		      G(x,1)=i;
+		      G(x,2)=j;
+		      G(x,3)=suma/min_column;
+		    else  
+		      x=x+1;
+		      G(x,1)=i;
+		      G(x,2)=j;
+		      G(x,3)=0;
+		    end
+		  end
+		end
+		G;
+		col_vector=sum(G,1); % sum of the elements of each column (size= 1 X 3)
+		result_cols=col_vector(1,3)/x;
+		suma_nodf_cols=col_vector(1,3);
+
+		%%% nestedness rows
+		G=zeros((rows*(rows-1))/2,3); % number of different pairs of rows
+		x=0;
+		for i=1: rows
+		  for j=i+1: rows
+		    suma=0;
+		    for k=1: columns
+		      suma=suma+(W(i,k)*W(j,k));
+		    end
+		    p=nnz(W(i,:)); % number of ones in the i row
+		    q=nnz(W(j,:)); % number of ones in the j row
+		    if p > q
+		      min_row=q;
+		      x=x+1;
+		      G(x,1)=i;
+		      G(x,2)=j;
+		      G(x,3)=suma/min_row;
+		    elseif p < q
+		      min_row=p;
+		      x=x+1;
+		      G(x,1)=i;
+		      G(x,2)=j;
+		      G(x,3)=suma/min_row;
+		    else
+		    x=x+1;
+		    G(x,1)=i;
+		    G(x,2)=j;
+		    G(x,3)=0;
+		    end
+		  end
+		end
+		G;
+		row_vector=sum(G,1); % sum of the elements of each column (size= 1 X 3)
+		result_rows=row_vector(1,3)/x;
+		suma_nodf_rows=row_vector(1,3);
+		nodf_whole_matrix(t,1)=(suma_nodf_cols+suma_nodf_rows)/(((columns*(columns-1))/2)+((rows*(rows-1))/2));
+	
+	t=t+1;
+	end
+end
+dlmwrite('result_equiprobable.txt', nodf_whole_matrix, ' ');
